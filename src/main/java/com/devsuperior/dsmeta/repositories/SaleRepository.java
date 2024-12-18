ae4cf86180bbc7e34773c9e7c3af11534ccb247a
Andre Proenca <andreproenca@hotmail.com>
2024-12-18 16:44:52 -0400

    JPQL searchSalesReport

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -3,6 +3,8 @@ package com.devsuperior.dsmeta.repositories;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -16,5 +18,10 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 			"WHERE obj.date BETWEEN :minDate AND :maxDate " +
 			"GROUP BY obj.seller.name")
 	List<SaleSummaryDTO> searchSalesSummary(LocalDate minDate, LocalDate maxDate);
+	
+	@Query("SELECT obj FROM Sale obj "
+			+ "WHERE (:name IS NULL OR UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name))) "
+			+ "AND obj.date BETWEEN :minDate AND :maxDate")
+	Page<Sale> searchSalesReport(String name, LocalDate minDate, LocalDate maxDate, Pageable pageable);
 
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -7,9 +7,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
@@ -32,5 +35,15 @@ public class SaleService {
 		LocalDate startDate = minDate != null ? LocalDate.parse(minDate) : endDate.minusYears(1L);
 		
 		return repository.searchSalesSummary(startDate, endDate);
+	}
+	
+	public Page<SaleReportDTO> searchSalesReport(String name, String minDate, String maxDate, Pageable pageable) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate endDate = maxDate != null ? LocalDate.parse(maxDate) : today;
+		LocalDate startDate = minDate != null ? LocalDate.parse(minDate) : endDate.minusYears(1L);
+		
+		Page<Sale> result = repository.searchSalesReport(name != null ? name : "", startDate, endDate, pageable);
+		
+		return result.map(x -> new SaleReportDTO(x));
 	}
 }
